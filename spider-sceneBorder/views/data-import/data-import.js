7ba42ccea6200b79f6cb749469ae3353d8368400
xiongyuqiong <yuqiong.xiong@huanuo-nsb.com>
2018-10-25 17:36:33 +0800

    导出

--- spider-sceneBorder/views/data-import/data-import.js
+++ spider-sceneBorder/views/data-import/data-import.js
@@ -1,3 +1,19 @@
- $(function() {
-    $( "#datepicker" ).datepicker();
-  });
+$(function() {
+	$("#datepicker").datepicker();
+});
+
+document.querySelector('#select_Folder').addEventListener('change', e => {
+  for (let entry of e.target.files)
+    console.log(entry.name, entry.webkitRelativePath);
+    
+$('#selectBtn').click(function() {
+	var selectDate = $("input[name='choose-date']:checked").val(); //获取选中的时间段
+	console.log(selectDate);
+	var selectDay=$("#datepicker").val(); //获取选中的具体日期
+	var selectProvince=$("#province").val();
+	var selectCity=$("#city").val();
+	var selectArea=$("#area").val();
+	var selectType=$("#select_type").val(); //获取选中的具体日期
+	var selectLat=$("#select_lat").val();
+	var selectLon=$("#select_lon").val();
+});
